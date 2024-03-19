604dbed705ffa1adc2f7748867336acd47ff112f
jericokatong <jericokatong@gmail.com>
2024-03-19 06:02:23 +0000

    FEAT: exercise unkpresent

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// define path constants
+define("APP_PATH", "http://103.59.94.247/unkpresent/public");
+
+// define database config
+define("HOST_DB", "localhost");
+define("NAME_DB", "db_attendance");
+define("USER_DB", "root");
+define("PASS_DB", "Cloud123#");
